b2bf757cfeb7043d1cd197a6c664671b72f512a3
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2025-10-31 10:48:22 +0100

    wifi is functionnal

--- stage2/05-reachy-mini/01-run-chroot.sh
+++ stage2/05-reachy-mini/01-run-chroot.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Cloning Reachy Mini repository..."
-SRC_DIR=/tmp/reachy_mini_code
-rm -Rf $SRC_DIR
-git clone --depth 1 -b 283-prepare-wireless-version https://github.com/pollen-robotics/reachy_mini.git $SRC_DIR
-echo "Repository cloned to $SRC_DIR."
-
 echo "Creating Python virtual environment..."
 rm -Rf /venvs
 sudo mkdir /venvs
@@ -15,7 +9,8 @@ python -m venv mini_daemon
 source mini_daemon/bin/activate
 
 echo "Installing Reachy Mini daemon..."
-pip install --upgrade --force-reinstall --no-cache-dir   "git+https://github.com/pollen-robotics/reachy_mini.git@283-prepare-wireless-version#egg=reachy_mini[wireless-version]"
+pip install --upgrade --force-reinstall --no-cache-dir "git+https://github.com/pollen-robotics/reachy_mini.git@prepare-wireless-version-2#egg=reachy_mini[wireless-version]"
+
 
 bash /venvs/mini_daemon/lib/python3.13/site-packages/reachy_mini/daemon/app/services/bluetooth/install_service_bluetooth.sh
 bash /venvs/mini_daemon/lib/python3.13/site-packages/reachy_mini/daemon/app/services/wireless/install_service.sh
@@ -27,10 +22,12 @@ for service in /etc/systemd/system/reachy-mini-daemon.service \
     fi
 done
 
+#echo "Enabling wifi activation service..."
+#ln -fs /etc/systemd/system/reachy-mini-activate-wifi.service /etc/systemd/system/multi-user.target.wants/reachy-mini-activate-wifi.service
+
 echo "Setting up restore state..."
 mkdir -p /restore/mini_daemon
 cp -r /venvs/mini_daemon/ /restore/mini_daemon
 echo "Restore state set up."
 
-echo "Cleaning up..."
-rm -Rf $SRC_DIR
+
