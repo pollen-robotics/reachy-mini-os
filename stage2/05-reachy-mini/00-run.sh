781e8a4546919ff95bdcd25965219100b480dc1a
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2025-11-01 18:52:35 +0100

    enhancement #3: triggre workflow on tag. add tag to VERSION.txt

--- stage2/05-reachy-mini/00-run.sh
+++ stage2/05-reachy-mini/00-run.sh
@@ -5,3 +5,9 @@ tar -xzf files/gst-plugins-rs-rpi.tar.gz -C ${ROOTFS_DIR}/
 # Set environment variable for GStreamer plugins
 echo 'export GST_PLUGIN_PATH=/opt/gst-plugins-rs/lib/aarch64-linux-gnu/' >> ${ROOTFS_DIR}/home/pollen/.bashrc
 echo "GStreamer plugins installed."
+
+echo "Creating VERSION.txt file..."
+rm ${ROOTFS_DIR}/home/pollen/VERSION.txt
+echo "ReachyMiniOS: dev" > ${ROOTFS_DIR}/home/pollen/VERSION.txt
+echo "Created on: $(date '+%Y-%m-%d')" >> ${ROOTFS_DIR}/home/pollen/VERSION.txt
+echo "VERSION.txt created."
