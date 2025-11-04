2d8f8487fe8f5e6006f58f73f71ed3fe4fcb116b
Pierre Rouanet <pierre.rouanet@gmail.com>
2025-11-04 10:16:14 +0100

    use git clone to get lfs assets

--- stage2/05-reachy-mini/01-run-chroot.sh
+++ stage2/05-reachy-mini/01-run-chroot.sh
@@ -15,8 +15,13 @@ cd /venvs
 runuser -u pollen -- uv venv mini_daemon --python 3.12
 source mini_daemon/bin/activate
 
+echo "Cloning reachy-mini repository..."
+runuser -u pollen -- git clone -b prepare-wireless-version-2 https://github.com/pollen-robotics/reachy_mini.git /venvs/src/reachy_mini
+runuser -u pollen -- git lfs install
+runuser -u pollen -- pushd /venvs/src/reachy_mini && git lfs pull && popd
+
 echo "Installing Reachy Mini daemon..."
-uv pip install --force-reinstall "git+https://github.com/pollen-robotics/reachy_mini.git@prepare-wireless-version-2#egg=reachy_mini[wireless-version]"
+uv pip install --force-reinstall "-e /venvs/src/reachy_mini[wireless-version]"
 
 mkdir -p /bluetooth
 
