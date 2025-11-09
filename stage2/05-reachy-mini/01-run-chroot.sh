4f08f73397fdfba9c29bda6c69442f3ef828ebd8
Fabien Danieau <fabien.danieau@pollen-robotics.com>
2025-11-09 20:52:01 +0100

    enhancement #25: clone branch with bluetooth and PIN

--- stage2/05-reachy-mini/01-run-chroot.sh
+++ stage2/05-reachy-mini/01-run-chroot.sh
@@ -18,7 +18,7 @@ source mini_daemon/bin/activate
 echo "Cloning reachy-mini repository..."
 git clone https://github.com/pollen-robotics/reachy_mini.git /venvs/src/reachy_mini
 cd /venvs/src/reachy_mini
-git checkout bbc2240d6f220b73f6fe76423d59161781bff679
+git checkout 380-add-pin-code-to-bluetooth
 chown -R pollen:pollen /venvs/src/
 
 echo "Installing Reachy Mini daemon..."
