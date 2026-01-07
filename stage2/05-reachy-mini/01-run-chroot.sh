348909d9466bf6210ef534455488a6ae18294edb
Pierre Rouanet <pierre.rouanet@gmail.com>
2026-01-07 11:45:32 +0100

    Update hash to point to reachy_mini 1.2.7

--- stage2/05-reachy-mini/01-run-chroot.sh
+++ stage2/05-reachy-mini/01-run-chroot.sh
@@ -18,7 +18,7 @@ source mini_daemon/bin/activate
 echo "Cloning reachy-mini repository..."
 git clone https://github.com/pollen-robotics/reachy_mini.git /venvs/src/reachy_mini
 cd /venvs/src/reachy_mini
-git checkout 9cb1543bf5b2c0ba73c7aa347ecfdab87508fa0e
+git checkout 1cf10f5af0ee7d7cd5f58a131468168b07f44104
 chown -R pollen:pollen /venvs/src/
 
 echo "Installing Reachy Mini daemon..."
